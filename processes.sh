9e6f3b9abc0763394e3e06daef6ef6bd9d998452
Theonetwofive1986 <145209374+theonetwofive1986@users.noreply.github.com>
2023-11-22 12:47:33 +0800

    Update processes.sh

--- processes.sh
+++ processes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#This will run the TOP command to get the running processes and save it to current directory as top.txt then display the output.
+#This will run the TOP command to get the running processes 
+#and save it to current directory as top.txt then display the output.
 set -e
 top=top.txt
 
