b1e1216f4a11050c40822c00c3eb9e62e94a1f98
Theonetwofive1986 <145209374+theonetwofive1986@users.noreply.github.com>
2023-11-23 12:22:43 +0800

    Update lastmodified.sh

--- lastmodified.sh
+++ lastmodified.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 #This script is to list the files inside /etc that has been modified then filtered using grep.
-#
+
+#To stop the script when encountered an error.
 set -e  
 
+#Create a variable that contains the output of find.
 lastmodified=lastmodified.txt
 
+#List a files and directories inside /etc and filter with grep then redirect the output to ./lastmodified.txt
 find /etc  -maxdepth 8 -type f,d  -ls | grep "Nov 22" > ./lastmodified.txt
 
+#Display the content of lastmodified.txt
 if [-f ~/lastmodified.txt]
 then
 cat $lastmodified
+else
+echo "Check the script."
 fi
