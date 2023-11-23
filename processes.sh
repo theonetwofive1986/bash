47373645c9a3b30dc2a4f989acc76d3b1938dc12
Theonetwofive1986 <145209374+theonetwofive1986@users.noreply.github.com>
2023-11-23 17:39:00 +0800

    Update processes.sh

--- processes.sh
+++ processes.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 #This will run the TOP command to get the running processes 
 #and save it to current directory as top.txt then display the output.
+
+
 set -e
+
+#Create a variable to store the output of TOP
 top=top.txt
 
+#Execute top and send the output to a file.
 top -b -n 1 > ./top.txt
 
+#Display the content of the output of top from the text file if existing.
 if [ -f ~/top.txt ]
 then
 cat $top
