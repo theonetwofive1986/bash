a08c0ef185808391e3078b9516304bc84740e9ad
Theonetwofive1986 <145209374+theonetwofive1986@users.noreply.github.com>
2023-11-23 16:08:07 +0800

    Create wifipassword.sh

---
+++ wifipassword.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#This is get the ssid and password of your saved Network in your laptop/computer.
+
+#"e" To stop the script when an error occured.
+#"x" To display the command during execution.
+#"v" To display the input command.
+set -evx
+
+#To remove the ssid.txt from your current directory if existing.
+#To enable this command remove the "#"
+#rm -f ./ssid.txt
+
+#To get the ssid and password on each network files in /etc/NetworkManager/system-connecitons/
+for file in /etc/NetworkManager/system-connections/*
+do
+  cat $file | grep "ssid=" >> ssid.txt;
+  cat $file | grep "ssid=" >> ssid.txt;
+done
