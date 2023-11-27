9a0069848c2df9d1d0d2ff7a2e001a796a211240
Theonetwofive1986 <145209374+theonetwofive1986@users.noreply.github.com>
2023-11-27 18:11:32 +0800

    Update lastmodified.sh

--- lastmodified.sh
+++ lastmodified.sh
@@ -7,7 +7,7 @@ set -e
 #Create a variable that contains the output of find.
 lastmodified=lastmodified.txt
 
-#List a files and directories inside /etc modified within the last 24 hours and redirect the output to ./lastmodified.txt
+#List files and directories inside /etc modified within the last 24 hours and redirect the output to ./lastmodified.txt
 #The output is sorted by date, -k is the field then 8 is the field number 1 is the character origin.
 find /etc  -maxdepth 5 -type f,d -newermt "-24 hour"  -ls | sort -k 8.1 > ./lastmodified.txt
 
