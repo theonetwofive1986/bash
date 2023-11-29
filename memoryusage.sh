4b8b69a46c5269fcf27591cb85ec448c8288a6ae
Theonetwofive1986 <145209374+theonetwofive1986@users.noreply.github.com>
2023-11-29 12:03:12 +0800

    Create memoryusage.sh

---
+++ memoryusage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#This is to display the processes and sorted by memory usage from high to low.
+
+#To display the executed command and stop when encountered an error.
+set -ex
+
+#To create a variable to store the result of top command.
+top=./top.txt
+
+#This will run and capture top and sort by Memory usage from high to low then save it to ./top.txt.
+top -b -n 1 -o +%MEM > ./top.txt
+
+if [ -f ~/top.txt ]
+then
+cat $top
+fi
