715303e55a8abd86300452e79b19c3d222d04bb3
Theonetwofive1986 <145209374+theonetwofive1986@users.noreply.github.com>
2023-11-29 15:45:34 +0800

    Create logs.sh

---
+++ logs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Create a variable to store the output of journalctl
+sudologs=sudologs.txt
+
+#Extract the system logs today and then show only those executed using sudo
+#then limit the output to 25 lines and save to sudologs.txt
+journalctl -S today | grep "sudo" | tail -n 25 > ./sudologs.txt
+
+#Display the sudologs.txt
+cat $sudologs 
