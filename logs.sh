04669f7e8adc741489f941c46e71e9d2981f41e3
Theonetwofive1986 <145209374+theonetwofive1986@users.noreply.github.com>
2023-12-03 11:10:43 +0800

    Update logs.sh

--- logs.sh
+++ logs.sh
@@ -4,7 +4,7 @@
 sudologs=sudologs.txt
 
 #Extract the system logs today and then show only those executed using sudo
-#then limit the output to 25 lines and save to sudologs.txt
+#then save to sudologs.txt
 journalctl -S today | grep "sudo" | tail > ./sudologs.txt
 
 #Display the sudologs.txt
